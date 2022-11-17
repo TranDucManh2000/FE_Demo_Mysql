ae958ca5a054f10082c094036c792c2003e44a83
manhtd <manh0982880741@gmail.com>
2022-11-17 11:19:15 +0700

    config axios

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../app/store";
 import ButtonCustom from "../../components/Button";
+import { request } from "../../config/libraries/axios";
 import useHome, { ReceivedProps } from "./hook";
 import { setCounter } from "./slice";
 import HomeWrapper from "./styled";
@@ -9,6 +10,18 @@ import HomeWrapper from "./styled";
 const HomeLayout: FC<ReceivedProps> = ({ current, setCurrent }) => {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
+
+  const demoAxios = () => {
+    request
+      .post("category", {
+        categoryName: "demoreact2",
+      })
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      });
+  };
+
   return (
     <HomeWrapper>
       <h1>{current}</h1>
@@ -16,6 +29,9 @@ const HomeLayout: FC<ReceivedProps> = ({ current, setCurrent }) => {
       <ButtonCustom onClick={() => dispatch(setCounter(5))}>
         set count
       </ButtonCustom>
+      <h2>-------</h2>
+      <h1>Axios</h1>
+      <ButtonCustom onClick={demoAxios}>Axios</ButtonCustom>
       <h2>-------</h2>
       <h1>Base64</h1>
     </HomeWrapper>
